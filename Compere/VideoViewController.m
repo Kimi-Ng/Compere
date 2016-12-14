c7bd08f96e68abbd0f66a56730624739f0fc7a4a
Kimi Ng <z.b.kimi@gmail.com>
2016-12-14 11:49:41 +0800

    Add video placeholder

--- Compere/VideoViewController.m
+++ Compere/VideoViewController.m
@@ -9,6 +9,7 @@
 #import "VideoViewController.h"
 
 @interface VideoViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *sentimentImageView;
 
 @end
 
@@ -23,15 +24,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
